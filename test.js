9f55719c44e143c266dec8b4befb35a399344194
Erica <Erica.Stanley@live.ca>
2018-07-18 10:35:29 -0400

    practice branching and merging

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-console.log('hello world')
+console.log('hello world');
 
-console.log('added this line as part of branch "iss1"')
+console.log('added this line as part of branch "iss1"');
+
+console.log('another line added for practice of branches; iss3');
