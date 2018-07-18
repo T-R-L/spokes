709b6c841cbe54dfda794d3a67e6932cd550f8dd
teoroderick <teo.roderick@gmail.com>
2018-07-18 11:17:44 -0400

    Added issue 5

--- test.js
+++ test.js
@@ -5,3 +5,4 @@ console.log('added this line as part of branch "iss1"');
 console.log('added a new console log - iss2');
 
 console.log('another line added for practice of branches; iss3');
+console.log('this line is for iss5');
